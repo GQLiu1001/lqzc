be2ea04ee46c5fa7c1accbe84c7452715d565ae1
RabbitTank <moondust1196551872@gmail.com>
2025-11-29 10:38:55 +0800

    Revert "temp:注释mail相关功能"
    This reverts commit 590cb85b0a2a9d69d55889b499a5a0eb12f621f0.

--- src/main/java/com/lqzc/config/MallInterceptor.java
+++ src/main/java/com/lqzc/config/MallInterceptor.java
@@ -1,29 +1,29 @@
-//package com.lqzc.config;
-//
-//import com.auth0.jwt.interfaces.DecodedJWT;
-//import com.lqzc.config.interceptor.AbstractAuthInterceptor;
-//import com.lqzc.utils.UserContextHolder;
-//import com.lqzc.utils.JwtUtils;
-//import jakarta.annotation.Resource;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//@Component
-//public class MallInterceptor extends AbstractAuthInterceptor {
-//    @Resource
-//    private JwtUtils jwtUtils;
-//
-//    @Override
-//    protected boolean doAuth(String token, HttpServletRequest request) {
-//        DecodedJWT decodedJWT = jwtUtils.verifyAndDecodeToken(token);
-//        if (decodedJWT == null) {
-//            return false;
-//        }
-//
-//        String cartId = jwtUtils.getCartId(decodedJWT);
-//        UserContextHolder.setCartId(cartId);
-//        return true;
-//    }
-//}
+package com.lqzc.config;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.lqzc.config.interceptor.AbstractAuthInterceptor;
+import com.lqzc.utils.UserContextHolder;
+import com.lqzc.utils.JwtUtils;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class MallInterceptor extends AbstractAuthInterceptor {
+    @Resource
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected boolean doAuth(String token, HttpServletRequest request) {
+        DecodedJWT decodedJWT = jwtUtils.verifyAndDecodeToken(token);
+        if (decodedJWT == null) {
+            return false;
+        }
+
+        String cartId = jwtUtils.getCartId(decodedJWT);
+        UserContextHolder.setCartId(cartId);
+        return true;
+    }
+}
